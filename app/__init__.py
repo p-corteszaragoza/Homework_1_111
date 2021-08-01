ef733184cbd82d891b899dff2d07b9fd1edf1bf8
p-corteszaragoza <pcorteszaragoza@gmail.com>
2021-08-01 12:27:06 -0700

    Adding the correct endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -6,12 +6,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/aboutme")
 def about_me():
     me = {
         "first_name":"Paola",
         "last_name":"Cortes",
-        "hobbies":"Paint and Tennis",
+        "hobbies":"Paint and Play Tennis",
         "test": True
 
     }
